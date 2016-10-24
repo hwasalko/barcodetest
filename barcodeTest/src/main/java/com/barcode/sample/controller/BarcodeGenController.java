04a08a48f3dc99fb54f97457af1f60bbc283a355
hwasalko <2dong0711@hanmail.net>
2016-10-24 10:54:48 +0900

    불필요한 import 제거

--- barcodeTest/src/main/java/com/barcode/sample/controller/BarcodeGenController.java
+++ barcodeTest/src/main/java/com/barcode/sample/controller/BarcodeGenController.java
@@ -1,9 +1,7 @@
 package com.barcode.sample.controller;
 
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.barcode.sample.services.BarcodeGenService;
 
